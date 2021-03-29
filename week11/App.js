7dcae1fa867d15795755e344a7c8f3effe5417c4
AlyanaT <AlyanaT@users.noreply.github.com>
2021-03-29 13:27:28 -0400

    fin

---
+++ week11/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Card, Button} from 'react-native-elements';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Card>
+        <Card.Title>Alyana Tavares</Card.Title>
+        <Text>Mac and Cheese</Text>
+        <Card.Divider/>
+        <Text>Crab Rangoons</Text>
+        <Button title="Submit"/>
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
